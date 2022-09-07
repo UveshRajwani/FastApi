977f152d1166576b45ffb29a979d6a995660574c
Uvesh Rajwani <uveshrajwani786@gmail.com>
2022-09-07 10:23:23 +0530

    bid HUB

---
+++ bid.py
@@ -0,0 +1,98 @@
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse
+from time import sleep
+from pydantic import BaseModel
+from typing import Optional
+from typing import List
+app = FastAPI()
+
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <h2>Your ID: <span id="ws-id"></span></h2>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var client_id = Date.now()
+            document.querySelector("#ws-id").textContent = client_id;
+            var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
+
+
+class WebUser():
+    def __init__(self, userid: int, websocket: WebSocket):
+        self.userid = userid
+        self.websocket = websocket
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+
+    def disconnect(self, user: WebUser):
+        self.active_connections.remove(user)
+
+    def add_to_active_list(self, user: WebUser):
+        self.active_connections.append(user)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str,):
+        for connection in self.active_connections:
+            await connection.websocket.send_text(message)
+            # if connection != websocket:
+            #     await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
+
+@app.get("/")
+async def get():
+    return HTMLResponse(html)
+
+
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    user = WebUser(userid=client_id, websocket=websocket)
+    manager.add_to_active_list(user)
+    try:
+        while True:
+
+            data = await websocket.receive_text()
+            print(manager.active_connections)
+            print(data, type(data))
+            await manager.broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(user)
+        await manager.broadcast(f"Client #{client_id} left the chat")
--- main.py
+++ main.py
@@ -1,87 +1,88 @@
-from typing import Optional
-from random import randint
-from fastapi import FastAPI, Response, status, HTTPException
-import pymongo
-from pydantic import BaseModel
-from bson.json_util import dumps, loads
+# from typing import Optional
+# from random import randint
+# from fastapi import FastAPI, Response, status, HTTPException
+# import pymongo
+# from pydantic import BaseModel
+# from bson.json_util import dumps, loads
+#
+# app = FastAPI()
+# uri = "mongodb://localhost:27017"
+# client = pymongo.MongoClient(uri)
+# db = client.fastapi
+# vg = db.posts
+#
+#
+# class Post(BaseModel):
+#     title: str
+#     content: str
+#     published: bool = True
+#     rating: Optional[int] = None
+#
+#
+# myPosts = [{'id': 815, 'title': 'Demo Post', 'content': 'Demo Content', 'published': True, 'rating': 9},
+#            {'id': 5, 'title': 'Demo Post 1', 'content': 'Demo Content', 'published': True, 'rating': 9}]
+#
+#
+# def find_post(id: int):
+#     try:
+#         result = list(vg.find({"id": id}, {"_id": 0}))
+#         return result[0]
+#     except IndexError:
+#         return None
+#
+#
+# @app.get("/")
+# async def root():
+#     return {"message": "Welcome to my api"}
+#
+#
+# @app.get("/hello/{name}")
+# async def say_hello(name: str):
+#     return {"message": f"Hello {name}"}
+#
+#
+# @app.post("/posts", status_code=status.HTTP_201_CREATED)
+# def create_post(new_post: Post):
+#     post_dict = new_post.dict()
+#     post_dict['id'] = randint(0, 999)
+#     print(post_dict['id'])
+#     insert_result = vg.insert_one(post_dict)
+#     return {"new_post": f"title {new_post.title}, content: {new_post.content} "}
+#
+#
+# @app.get("/posts")
+# def get_post():
+#     result = list(vg.find({}, {"_id": 0}))
+#     return result
+#
+#
+# @app.get("/posts/{id}")
+# def get_Post_By_Id(id: int, response: Response):
+#     post = find_post(id)
+#     if not post:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid post id")
+#     else:
+#         return post
+#
+#
+# @app.delete("/posts/{id}")
+# def delete_post(id: int):
+#     post = find_post(id)
+#     if not post:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid post id")
+#     else:
+#         vg.delete_one({"id":id})
+#         return {"message": f"successfully deleted your post with the id of {id}"}
+#
+#
+# @app.put("/posts/{id}")
+# def update_post(id: int, new_post: Post):
+#     post = find_post(id)
+#     if not post:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid post id")
+#     post_dict = dict(new_post)
+#     post_dict['id'] = id
+#     delete_post(id)
+#     vg.insert_one(post_dict)
+#     return post
 
-app = FastAPI()
-uri = "mongodb://localhost:27017"
-client = pymongo.MongoClient(uri)
-db = client.fastapi
-vg = db.posts
-
-
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
-    rating: Optional[int] = None
-
-
-myPosts = [{'id': 815, 'title': 'Demo Post', 'content': 'Demo Content', 'published': True, 'rating': 9},
-           {'id': 5, 'title': 'Demo Post 1', 'content': 'Demo Content', 'published': True, 'rating': 9}]
-
-
-def find_post(id: int):
-    try:
-        result = list(vg.find({"id": id}, {"_id": 0}))
-        return result[0]
-    except IndexError:
-        return None
-
-
-@app.get("/")
-async def root():
-    return {"message": "Welcome to my api"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_post(new_post: Post):
-    post_dict = new_post.dict()
-    post_dict['id'] = randint(0, 999)
-    print(post_dict['id'])
-    insert_result = vg.insert_one(post_dict)
-    return {"new_post": f"title {new_post.title}, content: {new_post.content} "}
-
-
-@app.get("/posts")
-def get_post():
-    result = list(vg.find({}, {"_id": 0}))
-    return result
-
-
-@app.get("/posts/{id}")
-def get_Post_By_Id(id: int, response: Response):
-    post = find_post(id)
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid post id")
-    else:
-        return post
-
-
-@app.delete("/posts/{id}")
-def delete_post(id: int):
-    post = find_post(id)
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid post id")
-    else:
-        vg.delete_one({"id":id})
-        return {"message": f"successfully deleted your post with the id of {id}"}
-
-
-@app.put("/posts/{id}")
-def update_post(id: int, new_post: Post):
-    post = find_post(id)
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid post id")
-    post_dict = dict(new_post)
-    post_dict['id'] = id
-    delete_post(id)
-    vg.insert_one(post_dict)
-    return post
